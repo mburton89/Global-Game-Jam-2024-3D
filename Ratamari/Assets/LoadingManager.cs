8df4813af812e027a00f1d4747d00899e956e9da
Matt Burton <matthewwburton@gmail.com>
2024-01-31 17:03:47 -0600

    Update LoadingManager.cs

--- Ratamari/Assets/LoadingManager.cs
+++ Ratamari/Assets/LoadingManager.cs
@@ -9,7 +9,7 @@ public class LoadingManager : MonoBehaviour
 
     public void Start()
     {
-        gameObject.transform.GetChild(0).gameObject.SetActive(true);
+        //gameObject.transform.GetChild(0).gameObject.SetActive(true);
     }
 
     public void LoadLevel()
